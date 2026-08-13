f4563fbcc177be5e6497f4a417d935f2e38ead5d
Adriano Abner Da Silva Bezerra <adriano.abner@live.com>
2026-08-13 20:34:23 -0300

    add: ex2

--- 00/ex7/ft_seed_inventory.py
+++ 00/ex7/ft_seed_inventory.py
@@ -8,3 +8,4 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
         print(f"{seed_type.capitalize()} seeds: {quantity} grams total")
     elif (unit == "area"):
         print(f"{seed} seeds: covers {quantity} square meters")
+# ta faltando coisa, ver PDF
---
+++ 01/ex2/ft_plant_growth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+class Plant:
+    def __init__(self, name: str,
+                 height: int,
+                 age: int,
+                 growth: int):
+        self.name = name
+        self.height = height
+        self.age_days = age
+        self.growth = growth
+
+    def show(self) -> None:
+        print(f"{self.name}:",
+              f"{self.height}cm,",
+              f"{self.age_days} days old",)
+
+    def age(self, days: int = 0) -> None:
+        if days < 0:
+            print("Negative days is invalid")
+            return
+        self.age_days += days
+
+    def grow(self, days: int = 1) -> None:
+        for _ in range(days):
+            self.height = round(self.height + self.growth, 2)
+
+
+if __name__ == "__main__":
+    plant = Plant("Rose", 25.0, 30, 0.8)
+
+    print("=== Garden Plant Growth ===")
+    plant.show()
+
+    initial_height = plant.height
+
+    for day in range(1, 8):
+        print(f"=== Day {day} ===")
+        plant.age(1)
+        plant.grow(1)
+        plant.show()
+
+    total_growth = round(plant.height - initial_height, 2)
+    print(f"Growth this week: {total_growth}cm")
